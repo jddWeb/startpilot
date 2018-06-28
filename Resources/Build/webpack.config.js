20c1861bbdb75b49d1931c0fc83583171efdcb8f
Boris Schauer <me@bschauer.de>
2018-06-28 11:05:19 +0200

    [TASK] Init Webpack

---
+++ Resources/Build/webpack.config.js
@@ -0,0 +1,23 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    entry: ['./Assets/Scss/main.scss'],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
